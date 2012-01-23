48de5d8b9a0979d2803de528147e332a9f290807
Mattia Panattoni <diaboliko-88-@hotmail.it>
2012-01-23 18:31:07 +0100

    15.12.2011 ParteUno

---
+++ DueActivity/src/pdm/pkg/mtt/Second.java
@@ -0,0 +1,14 @@
+package pdm.pkg.mtt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity {
+	
+	 public void onCreate(Bundle savedInstanceState) {
+		 
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.second);
+	    }
+
+}
