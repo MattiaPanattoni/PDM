7fb5eeae0e923b465495b0c8765108e4a88e912d
Mattia Panattoni <diaboliko-88-@hotmail.it>
2012-01-23 18:19:13 +0100

    17.01.2012 ParteUno

---
+++ DoToast/src/pdm/pkg/mtt/DoToastActivity.java
@@ -0,0 +1,42 @@
+package pdm.pkg.mtt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class DoToastActivity extends Activity implements OnClickListener{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      
+        
+        LinearLayout ll = new LinearLayout (DoToastActivity.this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,LayoutParams.FILL_PARENT));
+        
+        Button btn = new Button (this);
+        btn.setText("Saluta");
+        btn.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT ));
+        btn.setOnClickListener(this);
+        
+        ll.addView(btn);
+        setContentView(ll);
+    }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+			
+		Toast.makeText(getApplicationContext(), "Ciao a tutti", Toast.LENGTH_LONG).show();
+		
+	}
+
+}
+
